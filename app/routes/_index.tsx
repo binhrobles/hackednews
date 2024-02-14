4e8f03339783da4ededd170de40be0606198fc54
Binh <binhrobles@gmail.com>
2024-02-13 17:42:20 -0800

    separate hnclient, simple pagination

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,47 +3,22 @@
 
 import { useLoaderData } from '@remix-run/react';
 import { LoaderFunctionArgs, json } from '@remix-run/node';
-import { Story, StoryIdsResponse } from '~/types';
 import StoryList from '~/components/StoryList';
-
-const hackerNewsBaseUrl = 'https://hacker-news.firebaseio.com/v0';
-
-const viewToHackerNewsPath: Record<string, string> = {
-  ask: 'askstories',
-  home: 'beststories',
-  new: 'newstories',
-  show: 'showstories',
-  jobs: 'jobstories',
-};
+import HNClient from '~/clients/hackernews';
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   // parse the search params for `?view=`
   const url = new URL(request.url);
   const view = url.searchParams.get('view') || 'home';
-  const path = viewToHackerNewsPath[view];
+  const page = Number(url.searchParams.get('page')) || 1;
 
-  if (!path) {
+  try {
+    const stories = await HNClient.fetchStories({ view, page });
+    return json({ stories });
+  } catch (e) {
+    console.error(e);
     throw json('Page Not Found', { status: 404 });
   }
-
-  const storiesRes = await fetch(
-    `${hackerNewsBaseUrl}/${viewToHackerNewsPath[view]}.json`
-  );
-  const storyIds: StoryIdsResponse = await storiesRes.json();
-
-  const promises = storyIds
-    .slice(0, 10)
-    .map(async (storyId: number) => {
-      const storyRes = await fetch(
-        `${hackerNewsBaseUrl}/item/${storyId}.json`
-      );
-      const story: Story = await storyRes.json();
-      return story;
-    });
-
-  const stories = await Promise.all(promises);
-
-  return json({ stories });
 };
 
 export default function Index() {
