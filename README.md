1c1d82ef5cc9a54a0cfdcc6daa90f242ddc13215
Binh <binhrobles@gmail.com>
2024-02-19 14:40:11 -0800

    bring remix site up to the root

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Just another Hacker News clone, for learning.
 Tech used:
 
 - [Remix](remix.run) for fullstack JS framework
-  - with [Vite](vitejs.dev) for build + dev env
 - Tailwind + DaisyUI for styling
 - [SST](sst.dev) for local dev + deployment
 - Hacker News API for data
--- stacks/RemixStack.ts
+++ stacks/RemixStack.ts
@@ -2,9 +2,7 @@ import { RemixSite, StackContext } from 'sst/constructs';
 
 export function RemixStack({ stack }: StackContext) {
   // Create the Remix site
-  const site = new RemixSite(stack, 'Site', {
-    path: 'remix-site/',
-  });
+  const site = new RemixSite(stack, 'Site');
 
   // Add the site's URL to stack output
   stack.addOutputs({
