13279182934790b9c5e0153e9782f480e1d06a7d
Binh <binhrobles@gmail.com>
2024-02-21 01:19:39 +0000

    link hn.binhrobles.com cert, readme

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 ## Hacked News
 
-Just another Hacker News clone, for learning.
+[hn.binhrobles.com](hn.binhrobles.com)
+
+Just another Hacker News remix, for learning. Wanted to see historically popular posts. Without paying for some SQL DB, historical aggregation queries would've been pretty hard / expensive, so just went with a simple month picker vs current feed.
 
 Tech used:
 
-- [Remix](remix.run) for fullstack JS framework
-- Tailwind + DaisyUI for styling
+- [Remix](remix.run)
+- [Tailwind w/ DaisyUI](daisyui.com)
 - [SST](sst.dev) for local dev + deployment
-- Hacker News API for data
+- [Hacker News API](https://github.com/HackerNews/API) for live data
+- [hckrnews.com](hckrnews.com) endpoints for pulling historical data
--- stacks/RemixStack.ts
+++ stacks/RemixStack.ts
@@ -1,3 +1,4 @@
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 import { RemixSite, StackContext, use } from 'sst/constructs';
 import { BackendStack } from './BackendStack';
 
@@ -5,7 +6,20 @@ export function RemixStack({ stack }: StackContext) {
   const { table } = use(BackendStack);
 
   // Create the Remix site
-  const site = new RemixSite(stack, 'Site', { bind: [table] });
+  const site = new RemixSite(stack, 'Site', {
+    bind: [table],
+    customDomain: {
+      domainName: 'hn.binhrobles.com',
+      isExternalDomain: true,
+      cdk: {
+        certificate: Certificate.fromCertificateArn(
+          stack,
+          'HNCert',
+          'arn:aws:acm:us-east-1:152014527758:certificate/9300e347-ad37-4f68-aef8-f8c1024f7abb'
+        ),
+      },
+    },
+  });
 
   // Add the site's URL to stack output
   stack.addOutputs({
